9ea6e540f300ae1798fbf5412934949f417c63c4
cicomsa <ioanacomsa85@gmail.com>
2018-04-22 18:40:28 +0200

    check number of moves added

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,7 +1,5 @@
 import { JsonController, Get, Post, HttpCode, Body, Put, Param, NotFoundError, BadRequestError} from 'routing-controllers'
 import Games from './entity'
-import { Validate } from 'class-validator';
-import { ColorValidator } from './colorValidator';
 
 
 export const color = ["red", "yellow", "blue", "green", "magenta"]
@@ -39,34 +37,39 @@ export default class Controller {
   @Param('id') id: number,
   @Body() update: Partial<Games>
   ) {
+
   const game = await Games.findOne(id)
   if (!game) throw new NotFoundError('Cannot find game')
 
   const newBoard : string[] = []
   const oldBoard : string[] = []
+  const moves = (board1, board2) => 
+    board1
+      .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+      .reduce((a, b) => a.concat(b))
+      .length
   
-
   const stringBoard = JSON.stringify(game.board)
   const splitedBoard = stringBoard.split("")
-  const arrayBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
+  
   const arrayToOldBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
- 
   while(arrayToOldBoard.length) oldBoard.push(arrayToOldBoard.splice(0,3))
-  console.log(arrayBoard)
+  
+  const arrayBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
   const index = Math.floor((Math.random() * arrayBoard.length-1) + 1)
   arrayBoard.splice(index, 1, update.board)
   while(arrayBoard.length) newBoard.push(arrayBoard.splice(0,3))
-  
-  console.log(newBoard)
-  console.log(oldBoard)
-  
+    
   if (update.board) game.board = newBoard
+  if (moves(oldBoard, newBoard) !== 1) 
+    throw new BadRequestError('One move only, please')
+
   if (update.name) game.name=update.name
 
   game.color = color[Math.floor(Math.random() * color.length)]
   if (update.color) game.color=update.color
   if (game.color !== "red" && game.color!== "green" && game.color!== "yellow" && game.color!== "magenta" && game.color!== "blue") 
-  throw new BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only!')
+  throw new BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only, please!')
 
   return game.save()
   }
--- target/games/controller.js
+++ target/games/controller.js
@@ -36,28 +36,31 @@ let Controller = class Controller {
             throw new routing_controllers_1.NotFoundError('Cannot find game');
         const newBoard = [];
         const oldBoard = [];
+        const moves = (board1, board2) => board1
+            .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+            .reduce((a, b) => a.concat(b))
+            .length;
         const stringBoard = JSON.stringify(game.board);
         const splitedBoard = stringBoard.split("");
-        const arrayBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",");
         const arrayToOldBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",");
         while (arrayToOldBoard.length)
             oldBoard.push(arrayToOldBoard.splice(0, 3));
-        console.log(arrayBoard);
+        const arrayBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",");
         const index = Math.floor((Math.random() * arrayBoard.length - 1) + 1);
         arrayBoard.splice(index, 1, update.board);
         while (arrayBoard.length)
             newBoard.push(arrayBoard.splice(0, 3));
-        console.log(newBoard);
-        console.log(oldBoard);
         if (update.board)
             game.board = newBoard;
+        if (moves(oldBoard, newBoard) !== 1)
+            throw new routing_controllers_1.BadRequestError('One move only, please');
         if (update.name)
             game.name = update.name;
         game.color = exports.color[Math.floor(Math.random() * exports.color.length)];
         if (update.color)
             game.color = update.color;
         if (game.color !== "red" && game.color !== "green" && game.color !== "yellow" && game.color !== "magenta" && game.color !== "blue")
-            throw new routing_controllers_1.BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only!');
+            throw new routing_controllers_1.BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only, please!');
         return game.save();
     }
 };
