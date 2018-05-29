e30566a40e147b5984f28271fd54302ae81537a9
cicomsa <ioanacomsa85@gmail.com>
2018-05-29 15:34:29 +0200

    update.board error removed

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -33,42 +33,47 @@ export default class Controller {
   @Body() update: Partial<Games>
   ) {
 
-  const game = await Games.findOne(id)
-  if (!game) throw new NotFoundError('Cannot find game')
+    const game = await Games.findOne(id)
+    if (!game) throw new NotFoundError('Cannot find game')
 
-  const newBoard : string[][] = []
-  const oldBoard : string[][] = []
+    const newBoard : string[][] = []
+    const oldBoard : string[][] = []
 
-  // const moves = (board1, board2) => 
-  //   board1
-  //     .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
-  //     .reduce((a, b) => a.concat(b))
-  //     .length
+    const moves = (board1, board2) => 
+      board1
+        .map((row, y) => row.filter((cell, x) => board2[y][x] !== cell))
+        .reduce((a, b) => a.concat(b))
+        .length
+    
+    const stringBoard = JSON.stringify(game.board)
+    const splitedBoard = stringBoard.split("")
+    
+    const arrayToOldBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
+    while(arrayToOldBoard.length) oldBoard.push(arrayToOldBoard.splice(0,3))
+    
+    const arrayToNewBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
+    const index = Math.floor((Math.random() * arrayToNewBoard.length - 1) + 1)
   
-  const stringBoard = JSON.stringify(game.board)
-  const splitedBoard = stringBoard.split("")
-  
-  const arrayToOldBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
-  while(arrayToOldBoard.length) oldBoard.push(arrayToOldBoard.splice(0,3))
-  
-  const arrayToNewBoard = splitedBoard.filter(o => o !== "'" && o !== '"' && o !== "[" && o !== "]" && o !== ",")
-  const index = Math.floor((Math.random() * arrayToNewBoard.length - 1) + 1)
- 
-  arrayToNewBoard.splice(index, 1, update.board)
-  while(arrayToNewBoard.length) newBoard.push(arrayToNewBoard.splice(0,3))
-   
-  if (update.board) game.board = newBoard
-  // if (moves(oldBoard, newBoard) !== 1) 
-  //   throw new BadRequestError('One move only, please')
+    const strBoardUpdate = JSON.stringify(update.board)
+    const splitStrUpdate = strBoardUpdate.split(`\"`)
 
-  if (update.name) game.name=update.name
+    const boardUpdate = splitStrUpdate.join('')
 
-  game.color = color[Math.floor(Math.random() * color.length)]
-  if (update.color) game.color=update.color
-  if (game.color !== "red" && game.color!== "green" && game.color!== "yellow" && game.color!== "magenta" && game.color!== "blue") 
-  throw new BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only, please!')
+    arrayToNewBoard.splice(index, 1, boardUpdate)
+    while(arrayToNewBoard.length) newBoard.push(arrayToNewBoard.splice(0,3))
+    
+    if (update.board) game.board = newBoard
+    if (moves(oldBoard, newBoard) !== 1) 
+      throw new BadRequestError('One move only, please')
 
-  return game.save()
+    if (update.name) game.name=update.name
+
+    game.color = color[Math.floor(Math.random() * color.length)]
+    if (update.color) game.color=update.color
+    if (game.color !== "red" && game.color!== "green" && game.color!== "yellow" && game.color!== "magenta" && game.color!== "blue") 
+    throw new BadRequestError('Color: "red", "blue", "green", "yellow" or "magenta" only, please!')
+
+    return game.save()
   }
 }
 
